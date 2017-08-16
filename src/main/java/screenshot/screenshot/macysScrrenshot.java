1ce0448be003b3ace88f8ca150eb2a12502cd0fa
parag <parag@krawlnetpc-Vostro-3800>
2017-08-16 18:11:56 +0530

    screenshotaug16

---
+++ src/main/java/screenshot/screenshot/macysScrrenshot.java
@@ -0,0 +1,124 @@
+package screenshot.screenshot;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+
+import java.sql.DriverManager;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Enumeration;
+import java.util.Properties;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterTest;
+import org.testng.annotations.BeforeTest;
+import org.testng.annotations.Test;
+
+public class macysScrrenshot extends methods  {
+
+	
+
+	@BeforeTest
+	public void openBrowser() throws InterruptedException, IOException {
+
+		// OPEN chrome
+		System.setProperty("webdriver.chrome.driver", "/home/parag/Music/chromedriver");
+		driver = new ChromeDriver();
+		// driver = new FirefoxDriver();
+		driver.manage().window().maximize();
+		
+		/* String key="https://www.modaoperandi.com/,https://www.macys.com/";
+		 // the String to be split
+		String[] array = key.split(","); // split according to the hyphen and put them in an array
+		for(String subString : array){ // cycle through the array
+		   System.out.println(subString);
+		   driver.get(subString);
+		} */
+
+	
+		
+		//Dimension dimension = new Dimension(500, 300);
+		//driver.manage().window().setSize(dimension);
+		// open application
+		// driver.get("https://www.jcrew.com/in/");
+	}
+
+	@Test
+
+	public void scrrenShot() throws InterruptedException, IOException, SQLException {
+		
+
+		
+		try {
+			// Create FileInputStream Object
+			FileInputStream fileInput = new FileInputStream(new File("./properties/config.properties"));
+			// Create Properties object
+			Properties prop = new Properties();
+			// load properties file
+			prop.load(fileInput);
+			
+			
+			
+				Enumeration enuKeys = prop.keys();
+				while (enuKeys.hasMoreElements()) {
+//					for (Map.Entry<Object, Object> entry : prop.entrySet()) {
+//						   System.out.println(entry.getKey() + " = " + entry.getValue());
+						
+					String key1 = (String) enuKeys.nextElement();
+					String value = prop.getProperty(key1);
+					System.out.println(key1 + ": " + value);
+					driver.get(value);
+						   Thread.sleep(5000);
+						   xpath(key1);
+						   image(key1);
+						   file(key1);
+					}	  
+//				}
+			try{
+				java.sql.Connection conn = DriverManager.getConnection("jdbc:mysql://localhost/test","root","root");
+				//System.out.println("connection sucess");
+				String quiry = "SELECT * FROM student_table";
+				Statement stmt = conn.createStatement();
+				ResultSet rs = stmt.executeQuery(quiry);
+				while(rs.next()){
+					System.out.println("Name :"+rs.getString("s_class"));
+				}
+			}	 
+			catch (Exception e) {
+				e.printStackTrace();
+			}	 
+			
+			// driver.get("https://www.macys.com/");
+			
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+		
+		
+		
+		// Crew
+		 //driver.findElement(By.xpath(".//*[@id='global__header']/div[3]/div[1]/div/div[1]/div[6]/a[2]")).click();
+
+		
+		
+		
+		// driver.findElement(By.xpath("/html[@class=' js no-touch svg
+		// no-firefox']/body[@class='NavAppHomePage']/div[@id='tinybox']/div[@id='tinycontent']/div[@id='modal']/div[@id='local']/div[@id='notshipLink']/a")).click();
+
+		// capture screenshots
+		
+		
+		// Generate file
+		
+
+	}
+	
+	
+
+	@AfterTest
+	public void quit(){
+		driver.quit();
+	}
+
+}
---
+++ src/main/java/screenshot/screenshot/methods.java
@@ -0,0 +1,190 @@
+package screenshot.screenshot;
+
+import java.io.BufferedWriter;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.sql.Timestamp;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import java.util.Enumeration;
+import java.util.Properties;
+import org.apache.commons.io.FileUtils;
+import org.openqa.selenium.By;
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+
+public class methods {
+	WebDriver driver;
+	/* xpaths */
+
+	public void xpath(String retailer) throws InterruptedException, IOException {
+
+		try {
+			// Create FileInputStream Object
+			FileInputStream fileInput = new FileInputStream(new File("./properties/config1.properties"));
+			// Create Properties object
+			Properties prop = new Properties();
+			// load properties file
+			prop.load(fileInput);
+
+			Enumeration enuKeys = prop.keys();
+			while (enuKeys.hasMoreElements()) {
+
+				String key2 = (String) enuKeys.nextElement();
+				String value = prop.getProperty(key2);
+				System.out.println(key2 + ": " + value);
+				System.out.println(retailer + ":" + key2);
+				if (retailer.equals(key2)) {
+					driver.findElement(By.xpath(value)).click();
+					Thread.sleep(5000);
+
+				}
+
+			}
+
+			// driver.get("https://www.macys.com/");
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+		// try {
+		// // Create FileInputStream Object
+		// FileInputStream fileInput1 = new FileInputStream(new
+		// File("./properties/config1.properties"));
+		// // Create Properties object
+		// Properties prop1 = new Properties();
+		// // load properties file
+		// prop1.load(fileInput1);
+		// for(String xpath : prop1.stringPropertyNames()) {
+		// String value1 = prop1.getProperty(xpath);
+		// String[] array1 = value1.split(",");
+		// for(String subString1 : array1){
+		// System.out.println(subString1);
+		// // Moda operandi
+		// driver.findElement(By.xpath(subString1)).click();
+		//
+		// Thread.sleep(5000);
+		//
+		// }
+		//
+		// }
+		// // driver.get("https://www.macys.com/");
+		// Thread.sleep(5000);
+		// } catch (FileNotFoundException e) {
+		// e.printStackTrace();
+		// }
+		//
+	}
+
+	/* capture image */
+
+	public void image(String retailerr) throws IOException, InterruptedException, SQLException {
+		try {
+			// Create FileInputStream Object
+			FileInputStream fileInput = new FileInputStream(new File("./properties/config.properties"));
+			// Create Properties object
+			Properties prop = new Properties();
+			// load properties file
+			prop.load(fileInput);
+
+			Enumeration enuKeys = prop.keys();
+			while (enuKeys.hasMoreElements()) {
+
+				String key2 = (String) enuKeys.nextElement();
+				String value = prop.getProperty(key2);
+				System.out.println(key2 + ": " + value);
+				if (retailerr.equals(key2)) {
+					try {
+						Date datee = new Date();
+						SimpleDateFormat dateFormatt = new SimpleDateFormat("yyyy-MM-dd HH-mm");
+						TakesScreenshot ts = (TakesScreenshot) driver;
+						File source = ts.getScreenshotAs(OutputType.FILE);
+						FileUtils.copyFile(source,
+						new File("/var/www/html/screen/images/" + dateFormatt.format(datee) + " : " + key2 + ".png"));
+						
+						// sql connection
+						java.sql.Connection con = DriverManager.getConnection("jdbc:mysql://localhost/test", "root","root");
+
+						Statement st = con.createStatement();
+						// File imgfile = new File("./screenshot/" +
+						// dateFormatt.format(datee) +" : "+ key2+ ".png");
+
+						// FileInputStream fin = new FileInputStream(imgfile);
+
+						PreparedStatement pre = con.prepareStatement("INSERT INTO image_dataa ( date, name,image) VALUES ( ?, ?, ?)");
+						
+
+						pre.setTimestamp(1, new Timestamp(System.currentTimeMillis()));
+						pre.setString(2, key2);
+						pre.setString(3, dateFormatt.format(datee) + " : " + key2 + ".png");
+						// pre.setBinaryStream(3,(InputStream)fin,(int)imgfile.length());
+						pre.executeUpdate();
+						System.out.println("Successfully inserted the file into the database!");
+
+						pre.close();
+						con.close();
+
+					} catch (IOException e) {
+						System.err.println("Problem while taking  screenshot");
+					}
+				}
+
+			}
+
+			// driver.get("https://www.macys.com/");
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+	}
+
+	/* save source code */
+
+	public void file(String retailerrr) throws IOException {
+		try {
+			// Create FileInputStream Object
+			FileInputStream fileInput = new FileInputStream(new File("./properties/config.properties"));
+			// Create Properties object
+			Properties prop = new Properties();
+			// load properties file
+			prop.load(fileInput);
+
+			Enumeration enuKeys = prop.keys();
+			while (enuKeys.hasMoreElements()) {
+
+				String key2 = (String) enuKeys.nextElement();
+				String value = prop.getProperty(key2);
+				System.out.println(key2 + ": " + value);
+				if (retailerrr.equals(key2)) {
+					try {
+						Date date = new Date();
+						SimpleDateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd HH-mm-ss");
+						File statText = new File("./source/" + dateFormat.format(date) + " : " + key2 + ".txt");
+						BufferedWriter out = new BufferedWriter(new FileWriter(statText));
+						out.write(driver.getPageSource());
+						out.close();
+
+					} catch (IOException e) {
+						System.err.println("Problem while writing to the file");
+					}
+				}
+
+			}
+
+			// driver.get("https://www.macys.com/");
+
+		} catch (FileNotFoundException e) {
+			e.printStackTrace();
+		}
+
+	}
+}
